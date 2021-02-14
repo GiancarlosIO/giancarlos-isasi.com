ad7247da7cd611fd39b6d3008852ce7624472154
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2021-02-14 01:15:50 -0500

    feat: initial homepage layout

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+import Building from '../src/components/Building'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Mr. Nexus</h1>
+      <p className={styles.subtitle}>Website under construction</p>
+      <Building className={styles.building} />
+      <a className={styles.link} href="https://codepen.io/bungeedesign/pen/yMqZyv" target="_blank">codepen: https://codepen.io/bungeedesign/pen/yMqZyv</a>
+    </div>
+  )
+}
