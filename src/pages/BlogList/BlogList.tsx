3168cb63fb9bc9835dfe879196bbe56379123417
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2021-06-27 14:31:32 -0500

    feat: remove unnecessary char

--- src/pages/BlogList/BlogList.tsx
+++ src/pages/BlogList/BlogList.tsx
@@ -43,7 +43,7 @@ const BlogList: React.FC<BlogListProps> = ({ postList }) => {
           <Header />
           <div className="pt-4 lg:pt-20">
             <h1 className="font-bold text-center text-xl lg:text-3xl mb-4 lg:mb-0 text-yellow-500 dark:text-yellow-300">
-              {t('MAIN_TITLE').toUpperCase()}:
+              {t('MAIN_TITLE').toUpperCase()}
             </h1>
             <div
               className="mx-auto"
