5e47d9d1e944d2d6ae195a31ca44932c864e4c9f
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2021-12-12 17:08:39 -0500

    feat: replace animated-svg with static svg
    There was a bug in the lottlie-player that was creating a memory-leark bug

--- src/views/Homepage/Homepage.tsx
+++ src/views/Homepage/Homepage.tsx
@@ -1,30 +1,16 @@
 import * as React from 'react';
 import { useTranslation } from 'next-i18next';
 import { NextSeo } from 'next-seo';
-import dynamic from 'next/dynamic';
-import Media from 'react-media';
 
 import Header from '@/components/Header';
 import Container from '@/components/Container';
 
 import { PostPreview, Category } from '@/types';
 
-import { TWITTER_PROFILE } from '@/constants/social-media';
-import { SocialButtons, CategoriesChipList } from '@/components';
+import { CategoriesChipList } from '@/components';
 
-import { ManCoding } from './components';
-import { PostList } from './sections';
+import { PostList, Hero } from './sections';
 import { HomepageContextProvider } from './context';
-
-const WorkingManDynamic = dynamic(
-  () =>
-    import(
-      /* webpackChunkName: "working-main" */ './components/LottieAnimations/WorkingMan'
-    ),
-  {
-    ssr: false,
-  },
-);
 
 import styles from './Homepage.module.scss';
 
@@ -65,61 +51,7 @@ const Homepage: React.FC<HomepageProps> = ({ postList, categories }) => {
         <main className="relative mb-6 lg:mb-8">
           <Container className="relative z-20">
             <Header />
-            <div
-              className={`pt-2 sm:pt-3 lg:pt-10 lg:pb-12 mx-auto ${styles['hero-section']} ${styles.wrapper}`}
-            >
-              <div>
-                <section className="text-lg md:text-2xl lg:text-3xl font-bold">
-                  <p>{t('GRETTINGS')}</p>
-                  <div>
-                    <span>{t('IAM')}</span>{' '}
-                    <a
-                      href={TWITTER_PROFILE}
-                      rel="noreferrer"
-                      target="_blank"
-                      className="inline-block"
-                    >
-                      <h1 className="inline-block text-blue-600 hover:underline">
-                        {t('FULL_NAME')}
-                      </h1>
-                    </a>
-                    {', '}
-                    <span>{t('BIO_1')}</span>
-                  </div>
-                  <p className="my-4 font-normal text-base md:text-xl">
-                    {t('DESCRIPTION_1')}
-                  </p>
-                  <SocialButtons />
-                </section>
-              </div>
-              <Media
-                queries={{
-                  desktop: '(min-width: 1024px)',
-                }}
-                // set false for ssr
-                defaultMatches={{
-                  desktop: false,
-                }}
-              >
-                {matches => {
-                  return (
-                    <React.Fragment>
-                      {matches.desktop ? (
-                        <div
-                          className={`hidden lg:block w-full -mt-4 lg:-mt-12 lg:h-100 ${styles.animationWrapper}`}
-                        >
-                          <WorkingManDynamic />
-                        </div>
-                      ) : (
-                        <div className="block lg:hidden mt-8 mb-4">
-                          <ManCoding width="100%" height="100%" />
-                        </div>
-                      )}
-                    </React.Fragment>
-                  );
-                }}
-              </Media>
-            </div>
+            <Hero />
           </Container>
         </main>
         <main className="relative z-10">
